b49f0646097186589df900b442a470ea2b92f84f
krupalshah <krupalshah55@gmail.com>
2017-10-04 23:07:27 +0530

    fetching issues

--- source/fetch.py
+++ source/fetch.py
@@ -2,37 +2,47 @@ from config import *
 import urllib.parse
 import requests
 
-auth_token=""
+auth_token = None
 
 
 def __authenticate():
     try:
-        url = urllib.parse.urljoin(auth_url, client_id)
-        payload = {"client_secret": client_secret}
-        req = requests.put(url, payload)
+        payload = {
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "note": "bugs_by_lang"
+        }
+        req = requests.post(auth_url, data=payload)
+        req.raise_for_status()
         res = req.json
-        return res['token']
+        if debug:
+            print(res)
+        auth_token = res['token']
     except ValueError as err:
         print("Error occured while authenticating!")
-        print("trace : \n", err)
-        return None
-    except HTTPError as err:
-        return None
+        if debug:
+            print("trace : \n", err)
 
 
 def search_issues(url):
     if not url:
         return None
+    if not auth_token:
+        __authenticate()
     try:
-        req = requests.get(issues_url)
-
+        headers = {"access_token": auth_token}
+        req = requests.get(url, headers=headers)
+        req.raise_for_status()
+        res = req.json
+        return res
     except ValueError as err:
-        print("Error occured while authenticating!")
+        print("Unknown error occured while fetching data!")
         print("trace : \n", err)
         return None
-    except requests.exceptions.HTTPError as err:
-        auth_token = __authenticate()
-        search_issue(url)
+    except HTTPError as err:
+        print("Server error occured while fetching data!")
+        if debug:
+            print("error response : %s" % str(err.response))
 
 
 def write_response(file_name, response):
